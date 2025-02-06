45a96825ab3851f4869c1ab97ce885e08099c25a
Alin <alinalin03030@gmail.com>
2025-02-06 13:30:37 +0000

    sss

--- c++lessonOne/main1.cpp
+++ c++lessonOne/main1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <numeric>
+#include <iterator>
+#include <array>
+#include <algorithm>
 
 int main(){
+
+    // std::array<int, 100> ids;
+
+    // std::iota(std::begin(ids), std::end(ids), 0);
+
+    // for(int i : ids){
+    //     std::cout << i << std::endl;
+
+    // }
+
+    std::array<int , 3> myArray;
+    std::fill(std::begin(myArray), std::end(myArray), 1024);
+
+    for(int element : myArray){
+        std::cout << element << std::endl;
+    }
 
     return 0;
 }
