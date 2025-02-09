b4bb5d19d34b7210d6ce8b68b5a6fdbf2715da02
Alin <alinalin03030@gmail.com>
2025-02-09 19:17:46 +0000

    lab 7

--- PregatireExamenPOO/prog7.cpp
+++ PregatireExamenPOO/prog7.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+
 
 int main(){
+    std::ofstream fis;
+
+    fis.open("Fisier1.txt");
+
+    if(fis.is_open()){
+        fis << "Prima linie intr-un fisier." << std::endl;
+        fis.close();
+    } else {
+        std::cout << "Eroare la deschiderea fisierului" << std::endl;
+    }
 
     return 0;
 }
