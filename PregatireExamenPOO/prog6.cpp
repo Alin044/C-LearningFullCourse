671cd3a35770d2fde3d917d17511d696e1c1e983
Alin <alinalin03030@gmail.com>
2025-02-09 17:02:22 +0000

    laboratorul 6 cpp

--- PregatireExamenPOO/prog6.cpp
+++ PregatireExamenPOO/prog6.cpp
@@ -114,7 +114,102 @@ void Lista::afisare_lista(){
     }
 }
 
+class Supraincarcare{
+    public:
+        char producator[80];
+        int cod_produs;
+        char rezolutie[100];
+        char dimensiuni[100];
+    public:
+        friend std::ostream& operator<<(std::ostream& out, Supraincarcare &a);
+        friend std::istream& operator<<(std::istream& in,  Supraincarcare &a);
+};
+
+std::ostream& operator<<(std::ostream& out, Supraincarcare &a){
+    std::cout << "Vom citi datele pentru scanere si imprimante: " << std::endl;
+    std::cout << std::endl;
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Supraincarcare &a){
+    std::cout << "Introduceti Producatorul : ";
+    std::cin  >> a.producator;
+    std::cout << "Introduceti codul produsului : ";
+    std::cin  >> a.cod_produs;
+    std::cout << "Introduceti rezolutia : ";
+    std::cin  >> a.rezolutie;
+    std::cout << "Introduceti dimensiunile : ";
+    std::cin  >> a.dimensiuni;
+    return in;
+}
+
+void insert(Lista &l, int x){
+    int Tip_derivat;
+    char Soft_inclus[100];
+    int Viteza_scanare;
+    char Format[20];
+    char Tip[20];
+    ProduseMagazin* prodMag;
+
+    Supraincarcare supr;
+
+    std::cout << supr;
+    std::cin >> supr;
+
+    if(x == 0){
+        Scanere *Sc;
+        std::cout << "Introduceti Softul : ";
+        std::cin  >> Soft_inclus;
+        std::cout << "Introduceti viteza de scanare : ";
+        std::cin  >> Viteza_scanare;
+
+        Sc = new Scanere(1, supr.producator, supr.cod_produs, supr.rezolutie, supr.dimensiuni, Soft_inclus, Viteza_scanare);
+        prodMag = Sc;
+        l.adaugare(prodMag);
+    }else if(x == 1){
+        Imprimante *Im;
+        std::cout << "Introduceti formatul : ";
+        std::cin  >> Format;
+        std::cout << "Introduceti tipul : ";
+        std::cin  >> Tip;
+
+        Im = new Imprimante(2, supr.producator, supr.cod_produs, supr.rezolutie, supr.dimensiuni, Format, Tip);
+        prodMag = Im;
+        l.adaugare(prodMag);
+    }
+}
+
 int main(){
+    int opt;
+    Lista l;
+    l.head = nullptr;
+
+    do{
+        system("CLS");
+        std::cout << "1. Adaugare scanere in Lista. " << std::endl;
+        std::cout << "2. Adaugare imprimante in Lista. " << std::endl;
+        std::cout << "3. Afisare articole din magazin. " << std::endl;
+        std::cout << "0. Iesire. " << std::endl;
+        std::cout << "Selectati optiunea dorita : " ;
+        std::cin  >> opt;
+
+        switch(opt){
+            case 1:
+                insert(l, 0);
+                break;
+            case 2:
+                insert(l, 1);
+                break;
+            case 3:
+                l.afisare_lista();
+                break;
+            case 0:
+                //exit case 
+            default:
+                std::cout << "Unavailable option " << std::endl;
+                break;
+        }
+    }while(opt != 0);
 
     return 0;
 }
