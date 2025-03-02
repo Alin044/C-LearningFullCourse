0e86d8b21de657cc36b9c33500bd8439f5125d5e
Alin <alinalin03030@gmail.com>
2025-03-02 12:05:05 +0000

    auf

--- c++lessonClassesFriend/main1.cpp
+++ c++lessonClassesFriend/main1.cpp
@@ -2,7 +2,33 @@
 #include <vector>
 #include <string>
 
+class PrivateSecurityInfo{
+    friend class UDT;
+    private:
+        int passcode;
+};
+
+class UDT{
+    public:
+        UDT(){
+            m_privateVar = 0;
+        }
+        friend void printPrivateVariableMembers(UDT u);
+        //friend class PrivateSecurityInfo;
+    private:
+        PrivateSecurityInfo m_Info;  // this class is friend of UDT, so it can access private members of UDT
+        int m_privateVar;
+};
+
+
+void printPrivateVariableMembers(UDT u){
+    std::cout << "Private variable : " << u.m_privateVar << std::endl;
+}
+
 int main(){
+
+    UDT instance;
+
 
     return 0;
 }
