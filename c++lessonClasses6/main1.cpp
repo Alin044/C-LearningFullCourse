c57f046216526ecf8e3a771267646afac00df65d
Alin <alinalin03030@gmail.com>
2025-03-17 10:21:09 +0000

    auf

--- c++lessonClasses6/main1.cpp
+++ c++lessonClasses6/main1.cpp
@@ -2,12 +2,24 @@
 #include <initializer_list>
 
 struct UDT{
-    UDT(){
 
+    UDT(int a, int b, int c){
+        std::cout << "UDT constructor" << std::endl;
+        
+    }
+
+    UDT(std::initializer_list<int> data) : m_data(data){
+        std::cout << "Initializer list constructor " << std::endl;
+       // m_data = data;
     }
 
     ~UDT(){
 
+    }
+    void printData(){
+        for(auto& e : m_data){
+            std::cout << e << std::endl;
+        }
     }
 
     private:
@@ -15,6 +27,9 @@ struct UDT{
 };
 
 int main(){
+
+    UDT u{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    u.printData();
 
     return 0;
 }
